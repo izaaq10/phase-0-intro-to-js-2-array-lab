218340286b776ca7e3095e8c2fa83c7a61b16cec
Izaaq <odongoizaaq@gmail.com>
2024-03-23 09:39:56 -0400

    Adds the array

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Write your solution here!
+
+let cats = ["Milo", "Otis", "Garfield"];
+    
+
+
+
+function destructivelyAppendCat(){
+    console.log(cats.push("Ralph"))
+}``
+function destructivelyPrependCat(){
+    console.log(cats.unshift("Bob"))
+}
+
+function destructivelyRemoveFirstCat(){
+      console.log(cats.shift())
+}
+``
+function destructivelyRemoveLastCat(){
+       console.log(cats.pop())
+}
+
+
+let addCat = [...cats , "Broom"]
+    
+addCat;
+ 
